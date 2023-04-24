88890f981b50c99d732484a8a5990e37350e8344
Joao Luis Queijo Barroco Sobral <jqueijo-@c1r6s2.42lisboa.com>
2023-04-24 11:49:36 +0100

    updated split

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,37 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:01:50 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/04/24 11:25:42 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/04/24 11:48:59 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	substr_count(char const *s)
+static	int	substr_count(char const *s, int c)
 {
+	size_t	count;
+	size_t	in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && in_word == 0)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 1;
+		s++;
+	}
+	return (count);
 }
 
+static char	*fill_substr()
 static	ft_free();
-static	*fill_word();
+
+
 
 char	**ft_split(char const *s, char c)
 {
