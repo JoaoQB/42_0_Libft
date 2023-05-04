75a5bcd3570a4d8dc222db8c2d4abfb1cf7f59a2
jqueijo- <joaoluissobral@gmail.com>
2023-05-04 11:14:33 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:01:50 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/04/24 11:48:59 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/04/26 19:06:35 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,49 @@ static	int	substr_count(char const *s, int c)
 	return (count);
 }
 
-static char	*fill_substr()
-static	ft_free();
+static char	*fill_substr(const char *src, size_t len)
+{
+	char	*dest;
 
+	if (!src)
+		return (NULL);
+	dest = (char *)malloc(sizeof(char) * len);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, src, len);
+	return (dest);
+}
+
+static void	ft_free(char **strs, int count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (NULL);
+}
 
 
 char	**ft_split(char const *s, char c)
 {
+	char	split_strs;
+	size_t	i;
+	size_t	j;
+	size_t	i_word;
+
+	i = 0;
+	j = 0;
+	i_word = -1;
+	if (!s)
+		return (NULL);
+	split_strs = (char **)malloc((substr_count(s, c) + 1) * sizeof(char));
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && i_word < 0)
+	}
 }
