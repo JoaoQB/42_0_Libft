2504114a7f125a0439bead81c0ea697c21e2494f
Joao Luis Queijo Barroco Sobral <jqueijo-@c1r8s3.42lisboa.com>
2023-05-12 14:08:37 +0100

    fixed lstmap leaks. (Del content.)

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jqueijo- <jqueijo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 15:04:19 by jqueijo-          #+#    #+#             */
-/*   Updated: 2023/05/05 15:28:06 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2023/05/12 13:52:57 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*))
 {
 	t_list	*new_lst;
 	t_list	*new_node;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_lst = NULL;
 	while (lst != NULL)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
+			del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
